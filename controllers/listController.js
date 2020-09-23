cf3fef51128501364c67504037580dfceca939f7
Ronan <ronankff99@gmail.com>
2020-09-23 21:20:25 +0100

    Updated some commenting, need to fix styles

--- controllers/listController.js
+++ controllers/listController.js
@@ -1,3 +1,4 @@
+
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
@@ -15,11 +16,12 @@ var listSchema = new mongoose.Schema({
   item: String
 });
 
-//Create model
+//Create model using the precreated schema
 var List = mongoose.model('shopping', listSchema);
 
 //Controller functions
 module.exports = function(app){
+  //Get the list items from the DB using the List object
   app.get('/list', function(req, res){
     List.find({}, function(err, data){
       if (err) throw err;
@@ -28,13 +30,14 @@ module.exports = function(app){
   });
 
   app.post('/list', urlencodedParser, function(req, res){
-    var newItem = List(req.body).save(function(err, data){
+    List(req.body).save(function(err, data){
       if(err) throw err;
       res.render('list', {items: data});
     })
   });
 
   app.delete('/list/:item', function(req, res){
+    //Replaces the hyphen character with a space before searching for it and deleting it
     List.find({item: req.params.item.replace(/-/g," ")}).deleteOne(function(err, data){
       if(err) throw err;
       res.render('list', {items: data});
